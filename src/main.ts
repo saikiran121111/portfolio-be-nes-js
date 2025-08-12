6316958d2c02581432dfb468b2e96f4e5039e021
saikiran121111 <saikiranvsk3@gmail.com>
2025-08-12 13:31:40 +0530

    port update

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ config({ path: 'env/local/.env' });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3002);
 }
 bootstrap();
