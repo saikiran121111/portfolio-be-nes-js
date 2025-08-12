9cc4799f8a25b061b929bf2716f8aaf8cd4dea62
saikiran121111 <saikiranvsk3@gmail.com>
2025-08-12 15:49:01 +0530

    header versioning

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { VersioningType } from '@nestjs/common';
 
 // Load environment variables from the local .env file
 config({ path: 'env/local/.env' });
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  // Swagger setup
+function setupSwagger(app) {
   const config = new DocumentBuilder()
     .setTitle('Portfolio API')
     .setDescription('API documentation for Portfolio project')
@@ -17,6 +15,20 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
+}
+
+function headerVersioning(app) {
+  app.enableVersioning({
+    type: VersioningType.HEADER,
+    header: 'Version',
+  });
+}
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {cors: true});
+
+  headerVersioning(app);
+  setupSwagger(app);
 
   await app.listen(process.env.PORT ?? 3002);
 }
--- src/portfolio/portfolio.controller.ts
+++ src/portfolio/portfolio.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Version } from '@nestjs/common';
 import { PortfolioService } from './portfolio.service';
+import { HEADER_VERSION } from '../constants/headerVersion';
 
-@Controller('api/v1/portfolio')
+@Controller('api/portfolio')
 export class PortfolioController {
   constructor(private readonly portfolioService: PortfolioService) {}
 
   @Get('users') // GET /api/v1/portfolio/users
+  @Version(HEADER_VERSION)
   async getAllUsers() {
     return await this.portfolioService.getAllUsers();
   }
