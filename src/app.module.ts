7acde65a766abbf43cd98aa3af0e193e096a3768
saikiran121111 <saikiranvsk3@gmail.com>
2025-08-12 14:47:02 +0530

    structure creation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PortfolioModule } from './portfolio/portfolio.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [PortfolioModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
