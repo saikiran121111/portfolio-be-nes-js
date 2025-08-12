78957a96b1dba79c1ce10af4a0b26460f5669799
saikiran121111 <saikiranvsk3@gmail.com>
2025-08-12 14:58:52 +0530

    fetching user data

--- src/portfolio/portfolio.controller.ts
+++ src/portfolio/portfolio.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { PortfolioService } from './portfolio.service';
 
-@Controller('portfolio')
-export class PortfolioController {}
+@Controller('api/v1/portfolio')
+export class PortfolioController {
+  constructor(private readonly portfolioService: PortfolioService) {}
+
+  @Get('users') // GET /api/v1/portfolio/users
+  async getAllUsers() {
+    return await this.portfolioService.getAllUsers();
+  }
+}
