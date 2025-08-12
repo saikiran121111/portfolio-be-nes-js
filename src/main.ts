90d9525ce4a06ee1c40a97c2d350af13e07c1c08
saikiran121111 <saikiranvsk3@gmail.com>
2025-08-12 15:14:35 +0530

    swagger implementation

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 // Load environment variables from the local .env file
 config({ path: 'env/local/.env' });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('Portfolio API')
+    .setDescription('API documentation for Portfolio project')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(process.env.PORT ?? 3002);
 }
 bootstrap();
